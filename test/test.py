66cc2c54593ec18a553aaf1e1ae06b9498f75abb
RUSTelli <francescorasta02@gmail.com>
2025-04-04 15:52:58 +0200

    code refactor in test and seed_set

--- test/test.py
+++ test/test.py
@@ -1,4 +1,4 @@
-from methods.seed_set_methods import ALGORITHMS, greedy_seed_set, WTSS
+from methods.seed_set import ALGORITHMS, greedy_seed_set, WTSS
 from methods.cost_functions import FUNCTIONS, random_cost
 from methods.euristics import EURISTICS, f1
 from methods.cascade import influence_diffusion
@@ -7,9 +7,11 @@ import networkx as nx
 from shared.plotting import plot_graph_seed_set, plot_graph_influenced_nodes, get_graph_positions
 
 def main():
+    # Set up the graph and parameters.
     graph    = nx.read_gml(GRAPH_PATH)
     BUDGET   = 100
     plot     = True
+    GRAPH_CAMERA = get_graph_positions(graph)
 
     seed_set = WTSS(graph, BUDGET, random_cost, f1)
     influenced_nodes = influence_diffusion(graph, seed_set)
@@ -18,9 +20,8 @@ def main():
     print(f"Influenced nodes: {len(influenced_nodes)}")
     
     if plot:
-        pos = get_graph_positions(graph)
-        plot_graph_seed_set(graph, seed_set, pos)
-        plot_graph_influenced_nodes(graph, influenced_nodes, pos)
+        plot_graph_seed_set(graph, seed_set, GRAPH_CAMERA)
+        plot_graph_influenced_nodes(graph, influenced_nodes, GRAPH_CAMERA)
 
 if __name__ == "__main__":
     main()
--- test/test2.py
+++ test/test2.py
@@ -1,4 +1,4 @@
-from methods.seed_set_methods import ALGORITHMS
+from methods.seed_set import ALGORITHMS
 from methods.cost_functions import FUNCTIONS
 from methods.euristics import EURISTICS
 
@@ -7,18 +7,24 @@ from methods.cascade import influence_diffusion
 import networkx as nx
 from shared.plotting import plot_graph_seed_set, plot_graph_influenced_nodes, get_graph_positions
 
-graph = nx.read_gml(GRAPH_PATH)
-BUDGET = 100
-plot = True
+def main():
+    # Set up.
+    graph = nx.read_gml(GRAPH_PATH)
+    GRAPH_CAMERA = get_graph_positions(graph)
+    BUDGET = 100
+    plot = True
+    # also fix random seed?
 
 
-for algorithm in ALGORITHMS:
-    for cost_function in FUNCTIONS:
-        for euristic in EURISTICS:
-            seed_set = algorithm(graph, BUDGET, cost_function, euristic)
-            influenced_nodes = influence_diffusion(graph, seed_set)
+    for algorithm in ALGORITHMS:
+        for cost_function in FUNCTIONS:
+            for euristic in EURISTICS:
+                seed_set = algorithm(graph, BUDGET, cost_function, euristic)
+                influenced_nodes = influence_diffusion(graph, seed_set)
 
-            if plot:
-                pos = get_graph_positions(graph)
-                plot_graph_seed_set(graph, seed_set, pos)
-                plot_graph_influenced_nodes(graph, influenced_nodes, pos)
+                if plot:
+                    plot_graph_seed_set(graph, seed_set, GRAPH_CAMERA)
+                    plot_graph_influenced_nodes(graph, influenced_nodes, GRAPH_CAMERA)
+
+if __name__ == "__main__":
+    main()
